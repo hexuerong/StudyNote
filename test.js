928bffcbf2bb6a5cf04223ce87857c51c9ff216f
hexuerong-7f <hexuerong@7fgame.com>
2018-12-14 13:37:13 +0800

    示例

--- test.js
+++ test.js
@@ -1,2 +1,20 @@
-console.log('引入成功');
-export const a = 10;
+let a = 1,b = 2;
+let mod = {
+    Person:function(){
+        this.name = 'lili';
+    },
+};
+mod.Person.prototype.showName = function () {
+    console.log(this.name);
+    return this.name;
+};
+
+let show = function () {
+    console.log(a,b);
+};
+
+let sum = function () {
+    console.log(a+b);
+};
+export default mod;
+export {a,b,show,sum};
